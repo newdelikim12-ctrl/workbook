562cd162004c0a8cbbd5dc036b85f8db3a02accb
newdelikim12-ctrl <newdelikim12@gmail.com>
2026-09-25 22:18:43 +0900

    Add space between mobile countdown and note button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 단어 연습장 서비스워커
-const CACHE='wordbook-v13';
+const CACHE='wordbook-v14';
 const SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
