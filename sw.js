330d20a934e36baa8e5525f5013dce825f292e5f
newdelikim12-ctrl <newdelikim12@gmail.com>
2026-09-25 22:03:56 +0900

    Fix invisible card content and preserve only user-entered meaning line breaks

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 단어 연습장 서비스워커
-const CACHE='wordbook-v10';
+const CACHE='wordbook-v11';
 const SHELL=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
